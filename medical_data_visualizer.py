e41d3c44b975041377a4581266f180710d78d6a9
miborim <mirella.lima@sou.inteli.edu.br>
2024-09-20 13:50:22 -0300

    complete project

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -3,58 +3,54 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-# 1
-df = None
+# Read data from file
+df = pd.read_csv('medical_examination.csv')
 
-# 2
-df['overweight'] = None
+# Add an overweight column to the data
+df['overweight'] = df['weight']/ ((df['height'] / 100) ** 2)
+df['overweight'] = df['overweight'].apply(lambda x: 1 if x > 25 else 0)
 
-# 3
-
+# Normalize the data by making 0 always good and 1 always bad
+df['cholesterol'] = df['cholesterol'].apply(lambda x: 0 if x == 1 else 1)
+df['gluc'] = df['gluc'].apply(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
-    # 5
-    df_cat = None
+    # Melt the DataFrame
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
+    # Group the data by 'cardio' and 'value'
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value']).size().reset_index(name='total')
 
-    # 6
-    df_cat = None
-    
+    # Draw the catplot
+    fig = sns.catplot(x='variable', y='total', hue='value', col='cardio', data=df_cat, kind='bar').fig
 
-    # 7
-
-
-
-    # 8
-    fig = None
-
-
-    # 9
+    # Return fig
     fig.savefig('catplot.png')
     return fig
 
 
 # 10
 def draw_heat_map():
-    # 11
-    df_heat = None
+    # Clean data
+    df_heat = df[
+        (df['ap_lo'] <= df['ap_hi']) &
+        (df['height'] >= df['height'].quantile(0.025)) &
+        (df['height'] <= df['height'].quantile(0.975)) &
+        (df['weight'] >= df['weight'].quantile(0.025)) &
+        (df['weight'] <= df['weight'].quantile(0.975))
+    ]
 
-    # 12
-    corr = None
+    # Calculate the correlation matrix
+    corr = df_heat.corr()
 
-    # 13
-    mask = None
+    # Generate a mask for the upper triangle
+    mask = np.triu(np.ones_like(corr, dtype=bool))
 
+    # Set up the matplotlib figure
+    fig, ax = plt.subplots(figsize=(12, 8))
 
-
-    # 14
-    fig, ax = None
-
-    # 15
-
-
-
-    # 16
-    fig.savefig('heatmap.png')
+    # Draw the heatmap
+    sns.heatmap(corr, annot=True, fmt='.1f', mask=mask, cmap='coolwarm', 
+                vmax=0.3, center=0, square=True, linewidths=.5, cbar_kws={"shrink": .5}, ax=ax)
     return fig
